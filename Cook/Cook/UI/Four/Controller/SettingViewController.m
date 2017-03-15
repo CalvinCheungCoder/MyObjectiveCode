7b9c45f59180ff1f579397a8f3aa12a03e5f6c0e
张丁豪 <984382258@qq.com>
2017-03-15 15:57:48 +0800

    no message

--- Cook/Cook/UI/Four/Controller/SettingViewController.m
+++ Cook/Cook/UI/Four/Controller/SettingViewController.m
@@ -118,7 +118,8 @@
 
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     
-
+    [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
     if (indexPath.section == 0) {
         
         NSUInteger fileSize = [[SDImageCache sharedImageCache] getSize];
