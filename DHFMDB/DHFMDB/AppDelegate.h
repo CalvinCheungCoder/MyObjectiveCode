dd046c3dd750e5966868aeec756eaec39255706c
张丁豪 <984382258@qq.com>
2017-03-21 11:31:59 +0800

    Add DHFMDB

---
+++ DHFMDB/DHFMDB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DHFMDB
+//
+//  Created by 张丁豪 on 2017/3/21.
+//  Copyright © 2017年 张丁豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
